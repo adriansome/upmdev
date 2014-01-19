9bc8cd8a6103cfb872a81c473a8e6391bd0d0047
Adrian Somerfield <adriansome@gmail.com>
2014-01-19 21:07:45 +0000

    latest changes to home page
    latest changes to home page

--- index.php
+++ index.php
@@ -33,12 +33,17 @@
 
 <body id="home">
 	
-	<!-- Begin #page-header -->
+    <div id="wrapper">
+
+	<!-- Begin #home-header -->
+	<div id="home-header">
+            <!-- Begin #page-header -->
 	<header id="page-header">
 		<div class="constrained">
-			<a href="/" class="span-3"><img src="images/logo.png" alt="Morecraft Drury" class="logo" /></a>
+			<a href="/" class="span-3">
+                            <img src="" alt="university press marketing" class="logo" /></a>
 
-			<nav id="main-navigation" class="span-12">
+			<nav id="main-navigation" class="span-8">
 				<ul>
 					<li class="active"><a href="index_old.html">Home</a></li>
 					<li><a href="services.html">Services</a></li>
@@ -51,10 +56,7 @@
 		</div>
 	</header>
 	<!-- End #page-header -->
-
-	<!-- Begin #home-header -->
-	<div id="home-header">
-		<div class="constrained">
+        <div class="constrained">
 		<ul class="slider">
 			<li id="slide1">
 				<img src="example-content/home-slider1.jpg" alt="" />
@@ -101,53 +103,61 @@
 	<div style="position: absolute; left: 0px; top: 0px; opacity: 0.0;">
 		<div id="control-previous">Previous</div>
 		<div id="control-next">Next</div>
+	</div>	
 	</div>
-
-		<ul class="sub-navigation slide-controls">
-			<li><a href="#slide1">Programme &amp; Project Management</a></li>
-			<li><a href="#slide2">Quantity Surveying &amp; Commercial Management</a></li>
-			<li><a href="#slide3">Legal Advisers &amp; Claims Consultancy</a></li>
-			<li><a href="#slide4">Expert Witness &amp; Forensic Services</a></li>
-			<li><a href="#slide5">Strategic Procurement &amp; Management Consultancy</a></li>
-		</ul>
-	</div>
+        </div>
 	<!-- End #home-header -->
 
 	<!-- Begin #main-content-wrapper -->
 	<div id="main-content-wrapper">
 		<div class="constrained">
 			<div class="span-4 nugget">
-				<h2>Our Team</h2>
+				<h2>Latest Book Reviews</h2>
+                                <div class="book-review-holder">
+                               
+                                <div class="thumbnail">
+					<img src="example-content/book.png" alt="" width="60" height="86"/>
+				</div>
 				<div class="text">
-					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum et risus porta, aliquam sapien scelerisque, blandit quam. Morbi ut torto. aliquam sapien scelerisque, blandit quam. Morbi ut torto.</p>
+                                     <div class="book-title">01/02/03</div>
+					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                                            Vestibulum et risus porta, aliquam sapien scelerisque, </p>
+				 </div>
+                                    </div>
+                                    <div class="book-review-holder">
+                                      
+                                        <div class="thumbnail">
+					<img src="example-content/book.png" alt="" />
 				</div>
-				<div class="thumbnail">
-					<img src="example-content/nugget-team.png" alt="" />
+                                        <div class="text">
+                                              <div class="book-title">01/02/03</div>
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+                                    Vestibulum et risus porta, aliquam sapien scelerisque, </p>
 				</div>
-			</div>
+			    </div> </div>
+			
 
 			<div class="span-4 nugget">
-				<h2>Blog Entries</h2>
+                          
+				<h2>University Presses Marketing</h2>
+                                
 				<div class="date">01/02/03</div>
-				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum et risus porta, aliquam sapien scelerisque, </p>
-				<div class="date">01/02/03</div>
-				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum et risus porta, aliquam sapien scelerisque, </p>
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+                                    Vestibulum et risus porta, aliquam sapien scelerisque, </p>
+                               
+                            
+                                <div class="date">01/02/03</div>				
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                                    Vestibulum et risus porta, aliquam sapien scelerisque, </p>
 			</div>
+                            
 
-			<div class="span-7 nugget nugget-case-studies">
-				<h2>Case Studies</h2>
-				<ul class="items">
-					<li class="item">
-						<h3>Case Study 1</h3>
-						<p>Lorem ipsum dolor sit amet, consectetur adipiscingVestibulum et risus porta, aliquam sapien</p>
-						<a href="#" class="more">View full case study</a>
-					</li>
-					<li class="item">
-						<h3>Case Study 1</h3>
-						<p>Lorem ipsum dolor sit amet, consectetur adipiscingVestibulum et risus porta, aliquam sapien</p>
-						<a href="#" class="more">View full case study</a>
-					</li>
-				</ul>
+			<div class="span-4 nugget">
+				<h2>University Presses Marketing</h2>
+				<div class="date">01/02/03</div>
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum et risus porta, aliquam sapien scelerisque, </p>
+				<div class="date">01/02/03</div>
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum et risus porta, aliquam sapien scelerisque, </p>
 			</div>
 		</div>
 	</div>
@@ -156,26 +166,28 @@
 	<!-- Begin #page-footer -->
 	<footer id="page-footer">
 		<div class="constrained">
-			<div class="span-6">
-				<h2>About Us</h2>
+			<div class="span-4">
+				<h2>Contact us</h2>
 				<p>Estia Consulting is a Construction Consultancy, providing a wide range of construction and property advice to private, public, commercial and industrial clients.</p>
 				<p>We specialise in project management, quantity surveying, procurement, project recovery, dispute resolution and business management services. Our mission is to provide high value consultancy advice that will help improve our client’s performance, profits and future prosperity.</p>
+                                <p class="icon-facebook">.</p>
+
+				<p class="icon-twitter">.</p>
+				<p class="icon-google">.</p>
+
+				<p class="icon-email">.</p>
 			</div>
-			<div class="span-3"><!-- footer nav can go here --></div>
-			<div class="span-3"><!-- Twitter widget can go here --></div>
-			<div class="span-3">
-				<p class="icon-address">Castlemead, Lower Castle Street, Bristol, BS1 3AG</p>
+			<div class="span-4">
+                            <h2>Latest blogs</h2>Latest blogs</div>
+			
+			<div class="span-4">
+				 <h2>Latest tweets</h2>
 
-				<p class="icon-tel">0117 917 5200</p>
-
-				<p class="icon-address">Central Court, 25 Southampton Buildings, London, WC2A 1AL</p>
-				<p class="icon-tel">020 776 96781</p>
-
-				<p class="icon-email"><a href="mailto:enquires@morecraftdrury.com">enquires@morecraftdrury.com</a></p>
+				
 			</div>
 		</div>
 	</footer>
 	<!-- End #page-footer -->
-	
+	</div><!-- End #page-wrapper -->
 </body>
 </html>
